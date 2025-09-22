f67859d30b766963875b44624c0902a103581863
Ariska Adi Prasetyo <144831626+ariskaAdi@users.noreply.github.com>
2025-09-22 22:55:34 +0700

    Feature/ssg (#29)
    * update home ssg
    
    * search property ssg
    
    * update create property
    
    * update edit property

--- frontend/src/components/layouts/dashboard/dashboard-layout.tsx
+++ frontend/src/components/layouts/dashboard/dashboard-layout.tsx
@@ -4,12 +4,10 @@ import type React from "react";
 import { useState } from "react";
 import {
   LayoutDashboard,
-  HelpCircle,
   Settings,
   Menu,
   X,
   Building,
-  Bell,
   User,
   BarChart3,
   Store,
@@ -34,7 +32,10 @@ const sidebarUser = [
   { icon: LayoutDashboard, label: "Profile", link: "/dashboard" },
   { icon: Building, label: "Past Bookings", link: "/dashboard/history" },
   { icon: Grid3X3, label: "Bookings", link: "/dashboard/bookings" },
-  { icon: Bell, label: "Notifications", link: "/dashboard/notifications" },
+];
+
+const settingAccount = [
+  { icon: Settings, label: "Settings", link: "/dashboard/settings" },
 ];
 
 interface DashboardLayoutProps {
@@ -123,7 +124,26 @@ export function DashboardLayout({ children }: DashboardLayoutProps) {
                 })}
               </nav>
             </div>
-              <ButtonLogout/>
+            {/* Setting */}
+            <div>
+              <h3 className="text-sidebar-foreground/60 text-xs font-medium uppercase tracking-wider mb-3">
+                SETTING ACCOUNT
+              </h3>
+              <nav className="space-y-1">
+                {settingAccount.map((item, index) => (
+                  <a
+                    key={index}
+                    href={item.link}
+                    className="flex items-center gap-3 px-3 py-2 rounded-lg text-sm text-sidebar-foreground hover:bg-black/10 transition-colors">
+                    <item.icon className="w-5 h-5" />
+                    <span>{item.label}</span>
+                  </a>
+                ))}
+                <div className="mt-8">
+                  <ButtonLogout />
+                </div>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
